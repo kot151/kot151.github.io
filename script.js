1b91e599e2d5865b89f54e06fc038f0dcde766b5
kot151 <ya.kot151@yandex.ru>
2020-02-12 16:03:50 +0300

    add 2

---
+++ script.js
@@ -0,0 +1,19 @@
+function debit() {
+    var t1 = document.getElementById('time1').value.split(':');
+    var t2 = document.getElementById('time2').value.split(':');
+    var p1 = document.getElementById('pokaz').value;
+    
+    h1=+t1[0] * 60 + +t1[1];
+    h2=+t2[0] * 60 + +t2[1];
+    if (h1 > h2) {
+        deb = Math.round((1440 * p1) / (1440 - (h1 - h2)));
+    }
+    else if (h1 < h2) {
+        deb = Math.round((1440 * p1) / (1440 + (h2 - h1)));
+    }
+    else {
+        deb = p1;
+    }
+    document.getElementById('otvet').innerHTML = deb;
+    //alert(deb);
+}
