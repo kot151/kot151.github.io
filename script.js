01d78d7880f54acbe50baf3336e48fadaee6ddfd
kot151 <ya.kot151@yandex.ru>
2020-02-13 09:06:56 +0300

    upd 7

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function debit() {
-    var t1 = document.getElementById('time1').value.split(':');
-    var t2 = document.getElementById('time2').value.split(':');
+    var t1 = document.getElementById('time1').value;
+    //вернул tt1 и tt2, чтобы в таблице красиво выводило)
+    var tt1 = t1.split(':');
+    var t2 = document.getElementById('time2').value;
+    var tt2 = t2.split(':');
     var p1 = document.getElementById('pokaz').value;
     
-    h1=+t1[0] * 60 + +t1[1];
-    h2=+t2[0] * 60 + +t2[1];
+    h1=+tt1[0] * 60 + +tt1[1];
+    h2=+tt2[0] * 60 + +tt2[1];
     if (h1 > h2) {
         deb = Math.round((1440 * p1) / (1440 - (h1 - h2)));
     }
