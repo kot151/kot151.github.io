9e57ef7a1c305ed0545ca262582898c7917c4a2b
kot151 <ya.kot151@yandex.ru>
2020-02-25 14:39:23 +0300

    upd32

--- game/js/script.js
+++ game/js/script.js
@@ -1,34 +1,53 @@
 alert("Начинаем игру Космический Космос!");
 
+let windowW = document.documentElement.clientWidth;
+let windowH = document.documentElement.clientHeight;
+
+
 var left = 0;
 window.onkeydown = function move_left(event){
-	if(event.keyCode==37 & left != 0){
+	if(event.keyCode==37 && left != 0){
 		left=left-20;
 		}
-	else if(event.keyCode==39 & left != 800){
+	else if(event.keyCode==39 && left != windowW - +180){
 		left=left+20;
 		}
-		document.getElementById('myship').style.left = left + 'px';
-		document.getElementById("mykoord").innerHTML = left;
-	
+	else if(event.keyCode==32){
+        //пуляем от пробела
+
+	}
+	document.getElementById('myship').style.left = left + 'px';
+	document.getElementById('mykoord').innerHTML = left;
 };
 
 
-var elem = document.getElementById("enbullet");
+var elem = document.getElementById('enbullet');
 var enemy = document.getElementById('enemy');
-var pos = 0, back = true, bulletpos = 0,
+var enemypos = 0, back = true, bullethpos = 180,
 animation = setInterval(function(){
-	 pos += back ? 10 : -10;
-     if (pos>600||pos<0)  back = !back;
-	 enemy.style.left = pos + 'px';
-	 document.getElementById("enemykoord").innerHTML=pos;
+	 enemypos += back ? 10 : -10;
+     if (enemypos>windowW - +450||enemypos<0)  back = !back;
+	 enemy.style.left = enemypos + 'px';
+	 document.getElementById('enemykoord').innerHTML=enemypos;
 	 //полет пули вражеского звездолета
-	 if (bulletpos < 650) {
-		 bulletpos = bulletpos + 10;
-	 	 enbullet.style.top = bulletpos + 'px';
-		 enbullet.style.left = left + 'px';
+	 if (bullethpos < windowH - 250) {
+		 bullethpos = bullethpos + 10;
+	 	 enbullet.style.top = bullethpos + 'px';
+		 
 	 }
-	 else {bulletpos = 0};
+	 else {
+		bullethpos = 180;
+		enbullet.style.top = bullethpos + 'px';
+		enbullet.style.left = enemypos + +185 + 'px';
+		bulletvpos = enemypos;
+		document.getElementById('enbulvkoord').innerHTML = bulletvpos;		 
+	};
+	
+	document.getElementById('enbulhkoord').innerHTML = bullethpos;
+
+	if (bullethpos >= 660 && (bulletvpos >= left - +20 && bulletvpos <= left + +20)) {
+		alert('popadanie!')
+	};
 
 
 },50);
---
+++ test/js.js
@@ -0,0 +1,12 @@
+function showAlert() {
+    intervalID = setInterval(function1(), 500);
+};
+
+function function1(){
+    console.log('f1');
+};
+function function2(){
+    console.log('f2');
+};
+
+showAlert();
