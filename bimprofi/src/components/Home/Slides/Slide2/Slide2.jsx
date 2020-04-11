d7f7ee40ea19a6daa21bc952719facf862b76989
kot151 <ya.kot151@yandex.ru>
2020-04-12 00:06:10 +0300

    12042020

--- bimprofi/src/components/Home/Slides/Slide2/Slide2.jsx
+++ bimprofi/src/components/Home/Slides/Slide2/Slide2.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import AlertDialog from "./Dialog/AlertDialog";
 import style from "./Slide2.module.css";
 import support from "./../../../../img/icons/support.png";
 import pc from "./../../../../img/icons/pc.png";
@@ -7,7 +8,7 @@ import bitcoin from "./../../../../img/icons/bitcoin.png";
 import connect from "./../../../../img/icons/connect.png";
 import freelance from "./../../../../img/icons/freelance.png";
 
-class Slide2 extends Component {
+class Slide2 extends React.Component {
   render() {
     return (
       <div className={style.slide2}>
@@ -19,26 +20,38 @@ class Slide2 extends Component {
           <div className={style.pic}>
             <img className={style.img} src={support} />
             <p>Опытные проектировщики</p>
+            <AlertDialog
+              title="Опытные проектировщики"
+              text="Вашим проектом займутся опытные проектировщики со высшим образованием. У каждого из них сотни удачных проектов за спиной."
+            />
           </div>
           <div className={style.pic}>
             <img className={style.img} src={pc} />
             <p>Современное ПО</p>
+            <AlertDialog
+              title="Современное ПО"
+              text="В работе используется современное специализированное программное обеспечение. Также мы выдаём и весь исходный материал."
+            />
           </div>
           <div className={style.pic}>
             <img className={style.img} src={dollar} />
             <p>Конкурентные цены</p>
+            
           </div>
           <div className={style.pic}>
             <img className={style.img} src={bitcoin} />
             <p>Любые формы оплаты</p>
+            
           </div>
           <div className={style.pic}>
             <img className={style.img} src={connect} />
-            <p>Всегда на  связи: 24/7</p>
+            <p>Всегда на связи: 24/7</p>
+            
           </div>
           <div className={style.pic}>
             <img className={style.img} src={freelance} />
             <p>Удаленная работа</p>
+            
           </div>
         </div>
       </div>
